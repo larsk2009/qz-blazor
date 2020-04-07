0c2685778ba514d719a5c67c5bfdb023bb72186d
larsk2009 <larsk2009@live.nl>
2020-04-07 21:31:38 +0200

    Initial commit containing connect and IsConnected

---
+++ QzBlazor.Sample.ServerSide/Pages/Index.razor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using QzBlazor;
+
+namespace QzBlazor.Sample.ServerSide.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        private IJSRuntime JsRuntime { get; set; }
+
+        private Qz _qz;
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _qz = new Qz(JsRuntime);
+            }
+
+            if (!await _qz.IsConnected())
+            {
+                var result = await _qz.Connect();
+            }
+
+            var isConnected = await _qz.IsConnected();
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+    }
+}
---
+++ QzBlazor/QzBlazor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace QzBlazor
+{
+    public class Qz
+    {
+        private readonly IJSRuntime _runtime;
+        public Qz(IJSRuntime runtime)
+        {
+            _runtime = runtime;
+        }
+
+        public async Task<bool> Connect()
+        {
+            try
+            {
+                await _runtime.InvokeVoidAsync("qz.websocket.connect");
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        public async Task<bool> IsConnected()
+        {
+            try
+            {
+                return await _runtime.InvokeAsync<bool>("qz.websocket.isActive");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+    }
+}
