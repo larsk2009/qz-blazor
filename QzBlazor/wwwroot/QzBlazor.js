cbaf8e212d232a4e7a543ba0e8815ad8def6aa01
larsk2009 <larsk2009@live.nl>
2020-04-12 22:14:39 +0200

    Add initial custom js

---
+++ QzBlazor/wwwroot/QzBlazor.js
@@ -0,0 +1,9 @@
+﻿
+window.QzBlazor = {
+    assemblyname: "QzBlazor",
+    print: function (printerName, commands) {
+        let config = qz.configs.create(printerName);
+
+        qz.print(config, commands);
+    }
+};
