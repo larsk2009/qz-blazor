9fdb87fe0a502f4e796dfdd59bb60848a777feb9
larsk2009 <larsk2009@live.nl>
2020-05-01 17:14:43 +0200

    Add (unused) printerconfigs

---
+++ QzBlazor/Config/PrinterConfig.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using QzBlazor.Models;
+
+namespace QzBlazor.Config
+{
+    public class Config
+    {
+        private Config() { }
+
+        public object bounds { get; set; }
+        public string colorType { get; set; }
+        public int copies { get; set; }
+        public int density { get; set; }
+        public bool duplex { get; set; }
+        public object fallbackDensity { get; set; }
+        public string interpolation { get; set; }
+        public object jobName { get; set; }
+        public bool legacy { get; set; }
+        public int margins { get; set; }
+        public object orientation { get; set; }
+        public object paperThickness { get; set; }
+        public object printerTray { get; set; }
+        public bool rasterize { get; set; }
+        public int rotation { get; set; }
+        public bool scaleContent { get; set; }
+        public object size { get; set; }
+        public string units { get; set; }
+        public bool altPrinting { get; set; }
+        public object encoding { get; set; }
+        public object endOfDoc { get; set; }
+        public int perSpool { get; set; }
+    }
+
+    public class DirtyOpts
+    {
+    }
+
+    public class Printer
+    {
+        public string name { get; set; }
+    }
+
+    public class PrinterConfig
+    {
+        public Config config { get; set; }
+        public DirtyOpts _dirtyOpts { get; set; }
+        public Printer printer { get; set; }
+
+        public static async Task<PrinterConfig> GetConfigAsync(IJSRuntime jsRuntime, string printerName)
+        {
+            var config = await jsRuntime.InvokeAsync<PrinterConfig>("qz.configs.create", printerName);
+
+            return config;
+        }
+    }
+}
---
+++ QzBlazor/PrinterOptions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QzBlazor
+{
+    public class PrinterOptions
+    {
+        public Bounds Bounds;
+
+        public ColorType ColorType { get; set; }
+
+        public int Copies { get; set; }
+
+        public int Density { get; set; }
+
+        public DuplexOption DuplexOption { get; set; }
+
+        public int? FallbackDensity { get; set; }
+
+        public Interpolation Interpolation { get; set; }
+
+        public string JobName { get; set; }
+
+        public bool Legacy { get; set; }
+    }
+
+    public class Bounds
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    public enum ColorType
+    {
+        Color,
+        Grayscale,
+        Blackwhite
+    }
+
+    public enum DuplexOption
+    {
+        False,
+        OneSided,
+        Duplex,
+        LongEdge,
+        Tumble,
+        ShortEdge
+    }
+
+    public enum Interpolation
+    {
+        Bicubic,
+        Bilinear,
+        NearestNeighbor
+    }
+}
